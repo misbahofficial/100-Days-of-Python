a043d8787acf8397f033fa116935c760e93e6cf0
Misbah Ahmed <72851859+misbahofficial@users.noreply.github.com>
2023-06-03 22:30:21 +0600

    band name generator

---
+++ day01.py
@@ -0,0 +1,5 @@
+print("Welcome to beand name generator.")
+city_name = input("What's name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+
+print("Your brand name could be " + city_name + ' ' + pet_name)
