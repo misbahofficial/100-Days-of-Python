807bb04b25156de0a2589c7e94e415afe3518bb9
Misbah Ahmed <72851859+misbahofficial@users.noreply.github.com>
2023-07-21 11:31:16 +0600

    much shorter approach added

--- reversed_tuple.py
+++ reversed_tuple.py
@@ -17,3 +17,14 @@ def reversed_tuple_string(given_tuple):
 my_tuple = ('misbah', 'ahmed', 'razu')
 
 print(reversed_tuple_string(my_tuple))
+
+# This is a much shorter approach.
+
+def reverse_strings_in_tuple(input_tuple):
+    reversed_strings = tuple(string[::-1] for string in input_tuple)
+    return reversed_strings
+
+# Test the function
+input_tuple = ("hello", "world", "python")
+output_tuple = reverse_strings_in_tuple(input_tuple)
+print(output_tuple)
