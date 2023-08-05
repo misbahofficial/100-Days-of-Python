1d3d95fd6410d502537afe49b4f167c74299fa67
Misbah Ahmed <72851859+misbahofficial@users.noreply.github.com>
2023-08-05 09:53:15 +0600

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,11 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+def main():
+    print(factorial(10))
+
+if __name__ == "__main__":
+    main()
